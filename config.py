a5acf080d6de37b48aa1e5ffbb021c8c0ca389ad
SaikatGhosh1005677 <66582043+SaikatGhosh1005677@users.noreply.github.com>
2020-06-09 07:22:33 +0000

    m

---
+++ config.py
@@ -0,0 +1,6 @@
+ecs_test_drive = {
+    'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
+    'ecs_access_key_id' : '132358245284861317@ecstestdrive.emc.com',
+    'ecs_secret_key' : ' QO8mB+LDxsuHPxS+MXpheG/QQ8mQusCQC6jP9Tl0',
+    'ecs_bucket_name' : 'photo-album'
+}
---
+++ configold.py
@@ -0,0 +1,6 @@
+ecs_test_drive = {
+    'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
+    'ecs_access_key_id' : '132276173696851222@ecstestdrive.emc.com',
+    'ecs_secret_key' : ' QO8mB+LDxsuHPxS+MXpheG/QQ8mQusCQC6jP9Tl0',
+    'ecs_bucket_name' : 'photo-album'
+}
